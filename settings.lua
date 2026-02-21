0d9a1863abb12a7ca0ac6f32ae9ac66cb2a232e6
DoggoMan <40744700+DoggoMan@users.noreply.github.com>
2026-02-21 22:20:29 +1000

    feat: reduce default value to something more reasonable

--- settings.lua
+++ settings.lua
@@ -1,11 +1,9 @@
-data:extend({
-  {
+data:extend({{
     type = "int-setting",
     name = "dirty-launches-pollution-amount",
     setting_type = "runtime-global",
-    default_value = 50000,
+    default_value = 10000,
     minimum_value = 0,
     maximum_value = 1000000,
     order = "a"
-  }
-})
+}})
